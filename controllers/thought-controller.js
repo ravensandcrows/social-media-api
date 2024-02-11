fd58a85e798090873c3f90f73100247d12ea5032
ravensandcrows <blackstormkali@gmail.com>
2024-02-11 11:14:47 -0800

    updated thought branch

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -6,7 +6,7 @@ const thoughtController = {
     try {
       const thoughtData = await Thought.find().sort({ createdAt: -1 });
       res.json(thoughtData);
-    } 
+    }
     catch (err) {
       console.log(err);
       res.status(500).json(err);
@@ -20,7 +20,7 @@ const thoughtController = {
         return res.status(404).json({ message: "Oh no! There doesn't seem to be a thought!" });
       }
       res.json(thoughtData);
-    } 
+    }
     catch (err) {
       console.log(err);
       res.status(500).json(err);
@@ -42,7 +42,7 @@ const thoughtController = {
         return res.status(404).json({ message: 'Thought created but no user exists! Please sign in!' });
       }
       res.json({ message: 'You created a thought!' });
-    } 
+    }
     catch (err) {
       console.log(err);
       res.status(500).json(err);
@@ -51,14 +51,18 @@ const thoughtController = {
 
   // update thought
   async updateThought(req, res) {
-    const thoughtData = await Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $set: req.body }, { runValidators: true, new: true });
+    try {
+      const thoughtData = await Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $set: req.body }, { runValidators: true, new: true });
 
-    if (!thoughtData) {
-      return res.status(404).json({ message: "This thought doesn't seem to exist yet!" });
+      if (!thoughtData) {
+        return res.status(404).json({ message: "This thought doesn't seem to exist yet!" });
+      }
+      res.json(thoughtData);
     }
-    res.json(thoughtData);
-    console.log(err);
-    res.status(500).json(err);
+    catch (err){
+      console.log(err);
+      res.status(500).json(err);
+    }
   },
 
   // delete thought
@@ -81,7 +85,7 @@ const thoughtController = {
         return res.status(404).json({ message: "We can't delete this until we know who thunk this thought!" });
       }
       res.json({ message: 'Thought deleted!' });
-    } 
+    }
     catch (err) {
       console.log(err);
       res.status(500).json(err);
@@ -101,7 +105,7 @@ const thoughtController = {
         return res.status(404).json({ message: "You can't react to something that doesn't exist!" });
       }
       res.json(thoughtData);
-    } 
+    }
     catch (err) {
       console.log(err);
       res.status(500).json(err);
@@ -120,7 +124,7 @@ const thoughtController = {
         return res.status(404).json({ message: "There's no thought here!" });
       }
       res.json(thoughtData);
-    } 
+    }
     catch (err) {
       console.log(err);
       res.status(500).json(err);
